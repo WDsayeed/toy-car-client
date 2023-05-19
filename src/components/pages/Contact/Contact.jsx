326b2746c482a1cf593f5c6d668e637a63bac811
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-19 23:27:14 +0600

    added cotact sec

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -1,64 +1,96 @@
-import React from "react";
+import { FaMapMarkerAlt,FaMobileAlt } from "react-icons/fa";
+import { BsEnvelope } from "react-icons/bs";
 
 const Contact = () => {
   return (
     <div>
-      <div>
-        <div className="flex">
-          <div className="card w-96 bg-base-100 shadow-xl">
+        <h1 className="text-4xl text-center ">Contact <span className="text-[#7ca005]">Us</span></h1>
+      <div className="md:flex md:justify-between w-full md:container mx-auto">
+        
+          <div className="card w-full md:w-1/4 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-              <img
-                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                alt="Shoes"
-                className="rounded-xl"
-              />
+                <div className="btn btn-circle btn-outline hover:bg-[#ccea6a] hover:text-white hover:border-0 text-[#7ca005]">
+              <FaMapMarkerAlt></FaMapMarkerAlt>
+
+                </div>
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
+              <h2 className="card-title">OUR LOCATION</h2>
+              <p>(500) 123 556 789 / (600) 123 456 789</p>
+              <p>toycar@gmail.com</p>
             </div>
           </div>
+     
+        
+          <div className="card w-full md:w-1/4 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+              <div className="btn btn-circle btn-outline hover:bg-[#ccea6a] hover:text-white hover:border-0 text-[#7ca005]">
+                <FaMobileAlt></FaMobileAlt>
+              </div>
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">CONTACT US ANYTIME</h2>
+              <p>Mobile: 013 00 22 33</p>
+              <p>Fax: 123 456 789</p>
+            </div>
+          </div>
+       
+    
+          <div className="card w-full md:w-1/4 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+            <div className="btn btn-circle btn-outline hover:bg-[#ccea6a] hover:text-white hover:border-0 text-[#7ca005]">
+                <BsEnvelope></BsEnvelope>
+              </div>
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">WRITE SOME WORDS</h2>
+              <p>Service24/7@active.com info@active.com</p>
+            </div>
+          </div>
+        
+      </div>
+      <form className="md:container mx-auto my-16">
+      <h1 className="text-4xl">
+      <span className="text-[#96bf0e]">Get  In</span>  Touch 
+      </h1>
+      <div className="card-body grid grid-cols-2 gap-10">
+       
+        <div className="form-control ">
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-bordered w-full"
+          />
         </div>
-        <div>
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-              <img
-                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                alt="Shoes"
-                className="rounded-xl"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
-          </div>
+        
+        <div className="form-control ">
+          
+          <input
+            type="email"
+            placeholder="Email"   
+            className="input input-bordered w-full"
+          />
         </div>
-        <div>
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-              <img
-                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                alt="Shoes"
-                className="rounded-xl"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
-          </div>
+
+
+
+        <div className="form-control col-span-2 w-full">
+          
+          <input
+            type="text"
+          
+            placeholder="Write Message"
+       
+            className="input input-bordered w-full"
+          />
         </div>
       </div>
+      <div className="text-center w-full ">
+        <button className="btn btn-wide bg-[#CCD5AE] border-0 hover:text-[#69860c] hover:bg-[#FAEDCD] text-black">
+          SEND A MESSAGE
+        </button>
+      </div>
+    </form>
     </div>
   );
 };
